aa419668c80d5e6346138ae294861f7c0c227b84
ANMOL PANT <48082902+Anmol1109@users.noreply.github.com>
2021-04-10 23:08:01 +0530

    Add files via upload

---
+++ Solution.py
@@ -0,0 +1,12 @@
+T = int(raw_input())
+for i in range (0, T):
+    d = []
+    ans = 0
+    A = long(raw_input())
+    d = raw_input().split(' ')
+    ans = 1
+    for j in range (A-1):
+        d[j] = int(d[j])
+        ans = ans * d[j]
+        ans = ans % 1234567
+    print ans 
